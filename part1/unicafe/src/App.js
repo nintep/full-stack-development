e9a4a18eba1403c34b4a2a778cab154a8384f944
Nina Tepponen <nina.tepponen@aalto.fi>
2021-09-18 15:40:15 +0300

    reorder directory

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
+    </div>
+
+  )
+}
+
+export default App
